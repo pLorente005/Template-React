428813d97c639a141aa0d17b6a8010b6cb68b7cc
Plorente005 <paulorente005@gmail.com>
2024-10-28 23:50:46 +0100

    Commit de mis modificaciones

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
 const Colors = React.lazy(() => import('./views/theme/colors/Colors'));
 const Typography = React.lazy(() => import('./views/theme/typography/Typography'));
-const Contact = React.lazy(() => import('./views/theme/Contact/Contact')); // Nueva importación
+const Contact = React.lazy(() => import('./views/theme/Contact/Contact')); 
 
 // Base
 const Accordion = React.lazy(() => import('./views/base/accordion/Accordion'));
@@ -58,7 +58,7 @@ const routes = [
   { path: '/theme', name: 'Theme', element: Colors, exact: true },
   { path: '/theme/colors', name: 'Colors', element: Colors },
   { path: '/theme/typography', name: 'Typography', element: Typography },
-  { path: '/theme/Contact', name: 'Contact', element: Contact }, // Nueva ruta añadida aquí
+  { path: '/theme/Contact', name: 'Contact', element: Contact }, 
   { path: '/base', name: 'Base', element: Cards, exact: true },
   { path: '/base/accordion', name: 'Accordion', element: Accordion },
   { path: '/base/breadcrumbs', name: 'Breadcrumbs', element: Breadcrumbs },
